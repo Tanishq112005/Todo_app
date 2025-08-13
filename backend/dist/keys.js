e3947d82b18387231c9e739c7a5fd696e296415e
Tanishq Jain <tanishqjain1109@gmail.com>
2025-08-13 22:56:31 +0530

    .gitignore is updated

--- backend/dist/keys.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Mongo_db_connection = exports.Port_number = void 0;
-exports.Port_number = 3001;
-exports.Mongo_db_connection = "mongodb+srv://Drill_Endeavour:Tanishq%4022@hero.agqxn.mongodb.net/";
